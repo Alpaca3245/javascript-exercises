3ae4f4073416008758c3770ae90151fe2f83d7b7
Alpaca3245 <kyle.luu11@gmail.com>
2024-09-11 02:26:54 -0400

    Added index.html to 04 to test and finished the first problem for .spec.js for npm

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeArrayIndex) {
+    newArray = [];
+
+
+    for (let i = 0; i < array.length; i++) {
+        if (i == removeArrayIndex - 1) {
+            // Do nothing
+        } else {
+            newArray.push(array[i]);
+        }
+        console.log(i);
+        console.log(array.length);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
