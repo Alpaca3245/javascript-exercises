9913928d28ea66127df40a3af60d719bd1ea941f
Alpaca3245 <kyle.luu11@gmail.com>
2024-09-12 01:04:17 -0400

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let arraySum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    arraySum = arraySum + array[i];
+  }
+  return arraySum;
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+  // Can't initalize multiplySum as 0, otherwise it won't multiply no matter the number
+  // Initalized to first in array to prevent this from happening
+  let multiplySum = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+
+    multiplySum = multiplySum * array[i];
+  }
+  return multiplySum;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return (Math.pow(num1, num2));
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+
+	for (let i = 1; i <= num; i++) {
+      sum = sum * i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
