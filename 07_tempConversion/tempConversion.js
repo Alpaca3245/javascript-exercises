92730492a2595823cb89a2db8bbd3c682bd7c110
Alpaca3245 <kyle.luu11@gmail.com>
2024-09-11 22:00:38 -0400

    Finished 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    celsius = (fahrenheit - 32) * 5/9;
+    
+    // The "* 10) / 10" plus the ".round" rounds it to 1 decimal place
+    return Math.round(celsius * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+   fahrenheit = (celsius * 9/5) + 32;
+
+   // The "* 10) / 10" plus the ".round" rounds it to 1 decimal place
+   return Math.round(fahrenheit * 10) / 10 // 
 };
 
 // Do not edit below this line
