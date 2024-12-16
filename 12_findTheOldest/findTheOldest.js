c04f54418aea8d84a352c1bdb4f212e764386f07
Alpaca3245 <kyle.luu11@gmail.com>
2024-12-15 22:10:53 -0500

    Finished 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+// Using year only, get the age does not include months or day
+// Would of preferred to not make a new array, but I already looked at the solution and decided to use something else
+function getAge(arr) { 
+    if (Object.keys(arr).includes('yearOfDeath')) { // If key "yearOfDeath exists in arr"
+        return arr.yearOfDeath - arr.yearOfBirth; // calculates the age by yearOfBirth - yearOfDeath
+    } else { 
+        return (new Date().getFullYear()) - arr.yearOfBirth; // Create the current year if yearOfDeath does not exist (meaning still alive)
+    }
+}
 
+
+const findTheOldest = function(arr) {
+    return oldest = arr.reduce((oldestPerson, currentPerson) => {
+        oldestPersonAge = getAge(oldestPerson);
+        currentPersonAge = getAge(currentPerson);
+
+        return oldestPersonAge > currentPersonAge ? oldestPerson : currentPerson;
+    })
 };
 
 // Do not edit below this line
